8728a86c5d2412e63f224e0981a10746d004a9e3
vitaliy <vitaliy.yxz@gmail.com>
2012-11-27 23:12:09 +0200

    crate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwitchToolBackend::Application.config.secret_token = '33550d1a1a6026ae013e17b1212e8d07b255300a36e908e4338d96c0b3d6fa25c7fb9b96c6d21b32eb15cf8e7fb7e26372403eb684130de467ff677fc5fabb9c'
