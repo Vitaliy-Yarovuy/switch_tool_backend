ff44615ee82b81d10c3d8c3bf3ea1798ad30473d
Vitaliy-Yarovuy <vitaliy.yarovuy@qa-power.com>
2013-01-29 18:44:13 +0200

    update seeds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,18 +10,25 @@
 module ApplicationHelper
   def export_to_db()
     symptoms = Symptom.all
-    spec = ActiveRecord::Base.connection_config #Symptom.configurations["development"]
+    medicaments = Medicament.all
+    treatmentGroups = TreatmentGroup.all
+    treatments = Treatment.all
+    recommendations = ReplacementRecommendation.all
+
+    spec = ActiveRecord::Base.connection_config
     new_spec = spec.clone
     new_spec["database"] = "public/dbs/new.sqlite3"
-    ActiveRecord::Base.logger.info("[ApplicationHelper] before establish_connection publishing")
     ActiveRecord::Base.establish_connection new_spec #:publishing
     ActiveRecord::Migrator.migrate("db/migrate/", nil)
-    ActiveRecord::Base.logger.info("[ApplicationHelper] before copy")
-    copy_objs symptoms, Symptom
-    ActiveRecord::Base.logger.info("[ApplicationHelper] after copy")
+
+    #copy_objs symptoms, Symptom
+    copy_objs medicaments, Medicament
+    copy_objs treatmentGroups, TreatmentGroup
+    copy_objs treatments, Treatment
+    copy_objs recommendations, ReplacementRecommendation
 
     ActiveRecord::Base.establish_connection spec #:development
-    ActiveRecord::Base.logger.info("[ApplicationHelper] after establish_connection")
+
   end
 
   def copy_objs(objs, model)
@@ -31,15 +38,13 @@ module ApplicationHelper
       # remove any that will cause problems
       atts.delete("id")
       atts.delete("is_null")
-      # create the new object
-      new_o = model.new atts
-      # use send yo bypass mass assignment issues
-      # new_o.send :attributes=, atts, false
-      # set the id separately
+      #atts.delete("created_at")
+      #atts.delete("updated_at")
+      #atts.delete("ptoto_file_name")
+      #atts.delete("ptoto_content_type")tely
       new_o.id =o.id
       # make sure to save without validations
-      new_o.save_with_validation false
-      Rails.logger.error "COPIED #{ new_o.inspect }"
+      new_o.save
     }
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,73 +7,108 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require "C:\\win8\\switch_tool_backend\\extras\\paperclip_database_base64\\paperclip_database_base64"
+require Rails.root.join("extras\\paperclip_database_base64\\paperclip_database_base64")
 
 user = User.create({email: "v@v.ua", password: "111111", password_confirmation: "111111"})
+img_path = "C:\\win8\\switch-tool-win8\\switch-tool-win8\\switch-tool-win8\\images\\simptoms\\"
+
+texts =[
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum odio vel massa volutpat mollis. Phasellus dapibus auctor elit, porta egestas nisl rhoncus eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec tristique venenatis rhoncus. Pellentesque eu ligula magna, eu rhoncus enim. Proin quis nibh nulla, non mollis augue. Fusce sodales feugiat magna, et varius lacus adipiscing sagittis. Aenean aliquam velit eget justo consectetur ut iaculis diam vulputate. Nunc id odio at nunc consectetur semper vitae vestibulum odio. Phasellus sit amet lacus at metus rhoncus cursus. Vivamus posuere lacus eget sem porta vitae consectetur tellus suscipit. Vestibulum nec felis sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales condimentum enim quis auctor. Praesent magna urna, pellentesque vitae sodales non, scelerisque ut nibh.",
+    "eifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec tristique venenatis rhoncus. Pellentesque eu ligula magna, eu rhoncus enim. Proin quis nibh nulla, non mollis augue. Fusce sodales feugiat magna, et varius lacus adipiscing sagittis. Aenean aliquam velit eget justo consectetur ut iaculis diam vulputate. Nunc id odio at nunc consectetur semper vitae vestibulum odio. Phasellus sit amet lacus at metus rhoncus cursus. Vivamus posuere lacus eget sem porta vitae consectetur tellus suscipit. Vestibulum nec felis sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales condimentum enim quis auctor. Praesent magna urna, pellentesque vitae sodales non, scelerisque ut nibh.",
+    "Mauris at nisl sit amet lorem gravida lobortis. Praesent eget libero nec odio suscipit eleifend quis non ipsum. Proin cursus sagittis neque et interdum. Pellentesque vel turpis et risus bibendum aliquam. In hac habitasse platea dictumst. Aliquam sit amet mattis ante. Cras diam arcu, ultrices a faucibus a, adipiscing ut arcu. Sed ultricies fermentum aliquet. Cras tortor felis, vehicula sed porttitor non, blandit sit amet libero. Nunc id venenatis quam. Aenean aliquet justo non risus rhoncus tempor eget at felis. Pellentesque vitae lacus ipsum.",
+    "Nam vehicula tellus id neque pretium iaculis. Donec sit amet nibh velit, ac tincidunt ligula. Donec convallis, nulla pretium imperdiet pellentesque, massa lectus consectetur justo, nec cursus mauris ligula vitae orci. Aliquam in dolor velit. Aenean nibh nisl, dapibus a lobortis eget, accumsan ac eros. Suspendisse ut lacinia urna. Suspendisse eget tortor sit amet dolor rutrum tincidunt et in magna. Ut non sem vitae mauris auctor gravida ac quis sapien. Suspendisse porta quam auctor nisi tristique a aliquam massa malesuada. Phasellus placerat volutpat tellus in pharetra. Aenean sed ullamcorper tellus. Sed egestas congue neque vel mollis.",
+    "Curabitur laoreet magna sed odio accumsan pellentesque. Vivamus vel nibh ligula, eget imperdiet est. In nulla est, ullamcorper ac tincidunt eget, mollis vitae odio. Pellentesque rhoncus gravida eros, sit amet pretium metus malesuada sed. Nulla a euismod magna. Curabitur rhoncus laoreet dignissim. Praesent eleifend, turpis vel lacinia sodales, felis orci adipiscing eros, in bibendum ante nisi id elit. Aliquam erat volutpat. Vivamus a purus id justo molestie volutpat nec et est. Mauris consequat dapibus quam, in mollis eros rhoncus ut. Suspendisse id nibh justo, ac vestibulum leo. Suspendisse sodales tincidunt iaculis. Fusce quis blandit mi. Sed posuere lectus eu nulla gravida eget condimentum ligula eleifend. Praesent ac ligula turpis. Pellentesque non tellus nunc, at malesuada massa.",
+    "Aliquam vel bibendum erat. Donec sit amet vulputate odio. Integer ac eros eu libero dictum mattis vel porttitor ipsum. Mauris non imperdiet sapien. Nulla laoreet, enim quis posuere fringilla, nunc nibh malesuada urna, et interdum nisl enim ut nulla. Nulla mi libero, placerat in scelerisque non, viverra nec lectus. Morbi sapien mi, ullamcorper cursus congue nec, consectetur a enim. Quisque sem ipsum, suscipit id fringilla pulvinar, venenatis et lorem. Vestibulum vel purus et lacus porttitor ultrices. Phasellus viverra, risus vel adipiscing pellentesque, odio libero iaculis nunc, a ultricies ipsum tortor id sapien.",
+    "Mauris eleifend ipsum ac dui viverra auctor. Pellentesque a lorem magna, sit amet semper eros. Quisque a dictum ligula. Suspendisse urna eros, rhoncus et laoreet at, ornare sed arcu. Nam vitae mauris vel odio eleifend varius porttitor in erat. Nulla consectetur massa quis nisi ornare et eleifend purus pharetra. Curabitur id diam elit. Ut scelerisque nisi non elit placerat ut congue magna elementum. Nam lacinia, mi iaculis tincidunt dapibus, massa mi commodo nulla, feugiat adipiscing nulla mi vulputate est. Proin volutpat pulvinar lacus nec mollis.",
+    "Ut sit amet erat quis ligula elementum imperdiet at non libero. Nullam non volutpat turpis. Vivamus eget aliquet velit. Donec dignissim, erat non ornare dictum, sapien felis fermentum nibh, nec tempor ipsum velit vel libero. Maecenas tellus quam, commodo commodo mattis eget, fringilla a urna. Sed vel hendrerit mi. Ut cursus, lectus a lobortis condimentum, lacus erat ullamcorper diam, vel gravida velit justo ut velit. Curabitur quis nunc orci, sit amet mollis enim. Maecenas vitae velit erat. Cras quis mi ante, ac aliquam justo. Pellentesque volutpat consequat ornare. Curabitur eget erat vel mauris congue luctus.",
+    "Aenean in mi nec risus molestie auctor vitae et tortor. Vivamus sed dolor eros. Vivamus id orci eget ligula sodales elementum id a velit. Phasellus suscipit lectus non metus congue non tempor neque pretium. Integer a orci nisl. Ut lobortis imperdiet convallis. Aliquam erat volutpat. Fusce vel odio non quam tincidunt pharetra. Phasellus aliquam leo et sem ornare iaculis. Sed placerat leo sit amet magna congue gravida. Vivamus bibendum semper neque id porttitor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;"
+]
+
+
+class CircleEnumerator
+  include Enumerable
+  def initialize(arr)
+    @arr = arr
+  end
+  def each
+    n = 0
+    while true
+      yield @arr[n% @arr.length]
+      n += 1
+    end
+  end
+end
+
+
+text_gen = CircleEnumerator.new(texts).to_enum
+dose_gen = CircleEnumerator.new([1,2,3,4]).to_enum
+admission_gen = CircleEnumerator.new([1,2,3]).to_enum
 
 
 symptoms = Symptom.create([
-    {name: 'Простуда', description:'Головная боль Раздражение  слизистых  Температура Ломота', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_1.png')},
-    {name: 'Лихорадка', description:'Жар Температура Ломота', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_2.png')},
-    {name: 'Боли  в сердце', description:'Головная боль Раздражение', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_3.png')},
-    {name: 'Мигрень', description:'Усталость  Температура Ломота', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_4.png')},
-    {name: 'Боли  в животе', description:'Температура Ломота', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_5.png')},
-    {name: 'Температура', description:'Усталость Температура Ломота', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_6.png')},
-    {name: 'Звон в ушах', description:'Ломота', photo: File.open('C:\win8\switch-tool-win8\switch-tool-win8\switch-tool-win8\images\simptoms\simptom_7.png')},
+    {name: 'Простуда', description:'Головная боль Раздражение  слизистых  Температура Ломота', photo: File.open(img_path+'simptom_1.png')},
+    {name: 'Лихорадка', description:'Жар Температура Ломота', photo: File.open(img_path+'simptom_2.png')},
+    {name: 'Боли  в сердце', description:'Головная боль Раздражение', photo: File.open(img_path+'simptom_3.png')},
+    {name: 'Мигрень', description:'Усталость  Температура Ломота', photo: File.open(img_path+'simptom_4.png')},
+    {name: 'Боли  в животе', description:'Температура Ломота', photo: File.open(img_path+'simptom_5.png')},
+    {name: 'Температура', description:'Усталость Температура Ломота', photo: File.open(img_path+'simptom_6.png')},
+    {name: 'Звон в ушах', description:'Ломота', photo: File.open(img_path+'simptom_7.png')},
 ])
 
 medicaments = Medicament.create([
-    {name: 'Ацетилсалициловая кислота', small_description:'противовоспалительное,  обезбаливающее средство', drug_form: 1},
-    {name: 'Анальгин', small_description:'лекарственное средство, анальгетик  и антипиретик', drug_form: 2},
-    {name: 'Виагра', small_description:'восстанавливает нормальную реак- цию на сексуальное возбуждение', drug_form: 3},
-    {name: 'Метамизо́л на́трия', small_description:'лекарственное средство, анальгетик  и антипиретик', drug_form: 2},
-    {name: 'Кеторола́к', small_description:'нестероидный противовоспалитель- ный препарат', drug_form: 1},
-    {name: 'Дюфалак', small_description:'слабительное средство, употребля- еться при расстройствах желудка', drug_form: 2},
-    {name: 'Активированный уголь', small_description:'Дезинтоксикация при экзо- и эндоген- ных интоксикациях: диспепсия, метео- ризм, процессы гниения, брожения', drug_form: 1},
+    {name: 'Ацетилсалициловая кислота', small_description:'противовоспалительное,  обезбаливающее средство', drug_form: 1, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
+    {name: 'Анальгин', small_description:'лекарственное средство, анальгетик  и антипиретик', drug_form: 2, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
+    {name: 'Виагра', small_description:'восстанавливает нормальную реак- цию на сексуальное возбуждение', drug_form: 3, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
+    {name: 'Метамизо́л на́трия', small_description:'лекарственное средство, анальгетик  и антипиретик', drug_form: 2, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
+    {name: 'Кеторола́к', small_description:'нестероидный противовоспалитель- ный препарат', drug_form: 1, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
+    {name: 'Дюфалак', small_description:'слабительное средство, употребля- еться при расстройствах желудка', drug_form: 2, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
+    {name: 'Активированный уголь', small_description:'Дезинтоксикация при экзо- и эндоген- ных интоксикациях: диспепсия, метео- ризм, процессы гниения, брожения', drug_form: 1, big_description: text_gen.next , dosage_form: text_gen.next, medical_evidence: text_gen.next, trade_names: text_gen.next},
 ])
 
 treatmentsForFirst = Treatment.create([
-    {medicament: medicaments[0], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[2], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[3], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[0], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[2], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[3], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[6], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
 
 treatmentsForSecond = Treatment.create([
-    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[6], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[6], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
 
 treatmentsForThird = Treatment.create([
-    {medicament: medicaments[0], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[2], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[0], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[2], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
 
 treatmentsForFour = Treatment.create([
-    {medicament: medicaments[3], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[3], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
 
 treatmentsForFive = Treatment.create([
-    {medicament: medicaments[6], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[0], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[6], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[0], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
 
 treatmentsForSix = Treatment.create([
-    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[2], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[3], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[1], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[2], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[3], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
 
 treatmentsForSeven = Treatment.create([
-    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
-    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: 10,admission_per_day: 3 },
+    {medicament: medicaments[4], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
+    {medicament: medicaments[5], is_more_than_21: true, dose_by_taking: dose_gen.next, admission_per_day: admission_gen.next },
 ])
-
 
 
 treatmentGroups = TreatmentGroup.create([
