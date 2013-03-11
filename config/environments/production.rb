197ce6214e545050449895cbfddc99e7cfc17cab
vitaliy <vitaliy.yxz@gmail.com>
2013-03-11 22:30:13 +0200

    update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SwitchToolBackend::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -67,4 +67,5 @@ SwitchToolBackend::Application.configure do
 
   #path to dbs
   config.dbs_public_path = "dbs/"
+
 end
